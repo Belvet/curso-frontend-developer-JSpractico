3632530c2337832981103924eee95f543eedc68e
Martin Migueles <martin.migueles1996@gmail.com>
2023-05-08 21:51:50 -0300

    Arregle los errores de los menues al abrir, y logre que el menu del mail se abra cuando le pasas por encima, y tiene un timer, no se que hice pero funciona y queda bien

--- main.js
+++ main.js
@@ -1,10 +1,58 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const menuHamIcon = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuCarritoIcon = document.querySelector(".navbar-shopping-cart");
+const aside = document.querySelector(".product-detail");
 
-menuEmail.addEventListener("mouseover", toggleDesktopMenu);
-menuEmail.addEventListener("mouseout", toggleDesktopMenu);
+let desktopMenuTimer = null;
+
+menuEmail.addEventListener("mouseenter", toggleDesktopMenu);
+menuEmail.addEventListener("mouseleave", hideDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
+menuCarritoIcon.addEventListener("click", toggleCarritoAside);
+
+desktopMenu.addEventListener("mouseenter", stopHideDesktopMenu);
+desktopMenu.addEventListener("mouseleave", hideDesktopMenu);
+
 
 function toggleDesktopMenu() {
+    clearTimeout(desktopMenuTimer);
+    desktopMenu.classList.remove("inactive");
+}
 
-    desktopMenu.classList.toggle("inactive")
+function hideDesktopMenu() {
+    desktopMenuTimer = setTimeout(() => {
+        desktopMenu.classList.add("inactive");
+    }, 500);
+}
+
+function stopHideDesktopMenu(event) {
+    event.stopPropagation();
+    clearTimeout(desktopMenuTimer);
+}
+
+function toggleMobileMenu() {
+
+    const isAsideClosed = aside.classList.contains("inactive")
+
+    if (!isAsideClosed) {
+        aside.classList.add("inactive")
+    }
+
+    mobileMenu.classList.toggle("inactive")
+}
+
+function toggleCarritoAside() {
+
+    const isMobileMenuClosed = mobileMenu.classList.contains("inactive")
+
+        if (!isMobileMenuClosed) {
+            mobileMenu.classList.add("inactive")
+        }
+         
+        aside.classList.toggle("inactive");
+    
+
+    
 }
